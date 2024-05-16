ec7e553c1706a36b49c3e62595aa09e3dd6cf722
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2024-05-16 14:05:12 +0530

    css js update

--- js/main.js
+++ js/main.js
@@ -11,49 +11,49 @@
   });
 
   // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
+  // $(window).scroll(function() {
+  //   if ($(this).scrollTop() > 100) {
+  //     $('.back-to-top').fadeIn('slow');
+  //   } else {
+  //     $('.back-to-top').fadeOut('slow');
+  //   }
+  // });
+  // $('.back-to-top').click(function(){
+  //   $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+  //   return false;
+  // });
   
-	var nav = $('nav');
-	var navHeight = nav.outerHeight();
+	// var nav = $('nav');
+	// var navHeight = nav.outerHeight();
 
 	/*--/ ScrollReveal /Easy scroll animations for web and mobile browsers /--*/
-	window.sr = ScrollReveal();
-	sr.reveal('.foo', { duration: 1000, delay: 15 });
+	// window.sr = ScrollReveal();
+	// sr.reveal('.foo', { duration: 1000, delay: 15 });
 
 	/*--/ Carousel owl /--*/
-	$('#carousel').owlCarousel({
-		loop: true,
-		margin: -1,
-		items: 1,
-		nav: true,
-		navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
-		autoplay: true,
-		autoplayTimeout: 3000,
-		autoplayHoverPause: true
-	});
+	// $('#carousel').owlCarousel({
+	// 	loop: true,
+	// 	margin: -1,
+	// 	items: 1,
+	// 	nav: true,
+	// 	navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
+	// 	autoplay: true,
+	// 	autoplayTimeout: 3000,
+	// 	autoplayHoverPause: true
+	// });
 
 	/*--/ Animate Carousel /--*/
-	$('.intro-carousel').on('translate.owl.carousel', function () {
-		$('.intro-content .intro-title').removeClass('zoomIn animated').hide();
-		$('.intro-content .intro-price').removeClass('fadeInUp animated').hide();
-		$('.intro-content .intro-title-top, .intro-content .spacial').removeClass('fadeIn animated').hide();
-	});
+	// $('.intro-carousel').on('translate.owl.carousel', function () {
+	// 	$('.intro-content .intro-title').removeClass('zoomIn animated').hide();
+	// 	$('.intro-content .intro-price').removeClass('fadeInUp animated').hide();
+	// 	$('.intro-content .intro-title-top, .intro-content .spacial').removeClass('fadeIn animated').hide();
+	// });
 
-	$('.intro-carousel').on('translated.owl.carousel', function () {
-		$('.intro-content .intro-title').addClass('zoomIn animated').show();
-		$('.intro-content .intro-price').addClass('fadeInUp animated').show();
-		$('.intro-content .intro-title-top, .intro-content .spacial').addClass('fadeIn animated').show();
-	});
+	// $('.intro-carousel').on('translated.owl.carousel', function () {
+	// 	$('.intro-content .intro-title').addClass('zoomIn animated').show();
+	// 	$('.intro-content .intro-price').addClass('fadeInUp animated').show();
+	// 	$('.intro-content .intro-title-top, .intro-content .spacial').addClass('fadeIn animated').show();
+	// });
 
 	/*--/ Navbar Collapse /--*/
 	$('.navbar-toggle-box-collapse').on('click', function () {
@@ -84,34 +84,9 @@
 	});
 
 	/*--/ Property owl /--*/
-	$('#property-carousel').owlCarousel({
-		loop: true,
-		margin: 30,
-		responsive: {
-			0: {
-				items: 1,
-			},
-			769: {
-				items: 2,
-			},
-			992: {
-				items: 3,
-			}
-		}
-	});
 
 	/*--/ Property owl owl /--*/
-	$('#property-single-carousel').owlCarousel({
-		loop: true,
-		margin: 0,  
-		nav: true,
-		navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
+	
 
 	/*--/ News owl /--*/
 	$('#new-carousel').owlCarousel({
@@ -131,20 +106,6 @@
 	});
 
 	/*--/ Testimonials owl /--*/
-	$('#testimonial-carousel').owlCarousel({
-		margin: 0,
-		autoplay: true,
-		nav: true,
-		animateOut: 'fadeOut',
-		animateIn: 'fadeInUp',
-		navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
+	
 
 })(jQuery);
